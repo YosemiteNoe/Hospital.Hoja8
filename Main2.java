a737475517c3cbe673bc3e78ff5cfb565f2feb15
jonagr7 <agu14349@uvg.edu.gt>
2015-10-03 23:19:09 -0600

    Create Main2.java

---
+++ Main2.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.Vector;
+public class Main2 {
+	
+
+	static int contador = 0; 
+	static Vector<Paciente> v = new Vector<Paciente>();
+		  public static void leer(String archivo) throws FileNotFoundException, IOException {
+		        String cadena; 
+		        String nombre; 
+		        String enfermedad; 
+		        String prioridad; 
+		        FileReader f = new FileReader(archivo);
+		        BufferedReader b = new BufferedReader(f);
+		     try {
+		        while((cadena = b.readLine())!=null) { 
+		        	nombre = cadena.substring(0,cadena.indexOf(',')); // se guarda el nombre en el vector 1
+		        	enfermedad = cadena.substring(cadena.indexOf(',')+2,cadena.lastIndexOf(',')); // se guarda el nombre en el vector 2
+		        	prioridad = cadena.substring(cadena.lastIndexOf(',')+1,cadena.length()); // se guarda el nombre en el vector 3
+		        	v.add(new Paciente(nombre,enfermedad,prioridad));
+		        	contador++;
+		        }
+		        b.close();
+		     }
+		        catch (IOException e) {
+					e.printStackTrace();
+				}
+		    }
+		public static void main(String[] args) throws IOException {
+			// TODO Auto-generated method stub
+			leer("pacientes.txt"); // lectura del archivo pacientes
+			System.out.println("Jonathan Aguirre 14349 y Yosmith Melendez 14413");
+			VectorHeap2 heap = new VectorHeap2 (v);
+			for(int i=0; i<contador; i++){
+				Paciente paciente = (Paciente)heap.remove();
+				System.out.println(paciente.nombre+" "+","+paciente.enfermedad+" "+","+paciente.prioridad);
+			}
+
+			
+		}
+	}
